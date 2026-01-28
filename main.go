61c663c4ee2139f2fe54621aeb69b74b44baa5fd
n0needt0 <n0needt0@yahoo.com>
2026-01-27 16:10:04 -0800

    Fix NODE_NAME for Kubernetes: show node.pod instead of pod.pod

--- main.go
+++ main.go
@@ -114,16 +114,17 @@ func Run() error {
 		}
 
 		// Get actual hostname
-		// If running in Kubernetes with POD_NAME env var, use hostname.podname format
-		// This helps operators identify both the host and specific pod for debugging
+		// If running in Kubernetes with NODE_NAME env var, use node.pod format
+		// This helps operators identify both the node and specific pod for debugging
 		hostname, err := os.Hostname()
 		if err != nil {
 			log.Warnf("Failed to get hostname, using 'localhost': %v", err)
 			hostname = "localhost"
 		}
-		if podName := os.Getenv("POD_NAME"); podName != "" {
-			hostname = fmt.Sprintf("%s.%s", hostname, podName)
-			log.Infof("Running in Kubernetes, instance ID: %s", hostname)
+		if nodeName := os.Getenv("NODE_NAME"); nodeName != "" {
+			// In K8s: hostname is the pod name, NODE_NAME is the actual node
+			hostname = fmt.Sprintf("%s.%s", nodeName, hostname)
+			log.Infof("Running in Kubernetes on node %s, instance ID: %s", nodeName, hostname)
 		}
 
 		// Determine instance API URL without protocol (receiver webhook endpoint)
@@ -516,9 +517,9 @@ func cleanupStaleOperations(cfg *config.Config) {
 			instanceID = "receiver-unknown"
 		}
 	}
-	// If running in Kubernetes with POD_NAME env var, use hostname.podname format
-	if podName := os.Getenv("POD_NAME"); podName != "" {
-		instanceID = fmt.Sprintf("%s.%s", instanceID, podName)
+	// If running in Kubernetes with NODE_NAME env var, use node.pod format
+	if nodeName := os.Getenv("NODE_NAME"); nodeName != "" {
+		instanceID = fmt.Sprintf("%s.%s", nodeName, instanceID)
 	}
 
 	payload := map[string]string{
